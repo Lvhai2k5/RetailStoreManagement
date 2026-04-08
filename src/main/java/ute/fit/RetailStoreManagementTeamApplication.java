bbe5a3be5e5b3f7b94c867b2481846628821fa34
Lvhai2k5 <lehai332k5@gmail.com>
2026-04-09 00:15:22 +0700

    chore(init): setup structure RTLSTRMNGM-1 #done

---
+++ src/main/java/ute/fit/RetailStoreManagementTeamApplication.java
@@ -0,0 +1,13 @@
+package ute.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailStoreManagementTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailStoreManagementTeamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ute/fit/RetailStoreManagementTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package ute.fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailStoreManagementTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
